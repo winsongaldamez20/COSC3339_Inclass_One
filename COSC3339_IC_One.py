42c780ed3c624d7cb2563a118123605885bcdd97
winsongaldamez20 <winsongaldamez20@gmail.com>
2026-08-28 18:30:44 -0500

    Modify shipping cost calculation logic
    Updated shipping cost calculation for US destination to reflect new weight thresholds and base cost. Changed the cost and changed 2 changes which were the base cost and the weight if statement

--- COSC3339_IC_One.py
+++ COSC3339_IC_One.py
@@ -27,15 +27,15 @@ def count_words(sentence):
 
 # This method is long to allow for non-overlapping edits.
 def calculate_shipping_cost(weight, destination):
-    cost = 0.0
+    cost = None
     
     if destination == "US":
-        base_cost = 5.0
-        if weight <= 10:
+        base_cost = 6.0
+        if weight <= 8:
             cost = base_cost
         else:
             # Over 10 lbs, add $1 per extra lb
-            extra_weight = weight - 10
+            extra_weight = weight - 8
             cost = base_cost + (extra_weight * 1.0)
             
     elif destination == "International":
